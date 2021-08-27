ce3ec50853fb21ec6cd6660e5c9c2647893ca381
MarcoAR1 <marcorivero.mr26@gmail.com>
2021-08-27 20:02:26 -0300

    restore db

--- back-end/src/db.ts
+++ back-end/src/db.ts
@@ -11,15 +11,10 @@ import {
   DB_NAME,
 } from './utils/config'
 import Models from './models/index'
-import { IClient } from 'pg-promise/typescript/pg-subset'
-class DATABASE {
+export default class DATABASE {
   public static DB = pg()(this.getCN())
-  public DB: PgType
 
-  public async initDatabase(): Promise<void> {
-    const res = await DATABASE.DB.connect()
-    console.log('database connected')
-    this.DB = res
+  public static async initDatabase(): Promise<void> {
     if (NODE_ENV !== DEVELOPMENTMODE) {
       console.log(
         `Database ${DB_NAME} is not in development mode try create models`
@@ -30,12 +25,12 @@ class DATABASE {
   }
 
   private static getCN(): string {
-    return `postgres://${DB_USER}:${decodeURIComponent(`${DB_PASSWORD}`)}@${
+    return `postgres://${DB_USER}:${DB_PASSWORD}@${
       NODE_ENV === TEST ? DB_HOST_TEST : DB_HOST
     }:${DB_PORT ? +DB_PORT : 5432}/${
       NODE_ENV === TEST ? DB_NAME_TEST : DB_NAME
     }`
   }
 }
-export default new DATABASE()
-export type PgType = pg.IConnected<Record<string, unknown>, IClient>
+
+export type PgType = typeof DATABASE.DB
