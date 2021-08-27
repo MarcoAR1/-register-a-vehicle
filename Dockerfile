fe313372b6046e4c12b5ebd4a7ddd9da0aafdadb
MarcoAR1 <marcorivero.mr26@gmail.com>
2021-08-27 17:02:41 -0300

    change cn get

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:16-alpine3.11
 WORKDIR /app
 COPY . .
-RUN npm install -g npm@7.21.1
-RUN npm install
-RUN npm run build
+RUN npm install -g npm@7.21.1 \
+    && npm install \
+    && npm run build
 CMD ["npm","start"]
--- back-end/src/db.ts
+++ back-end/src/db.ts
@@ -12,19 +12,25 @@ import {
 } from './utils/config'
 import Models from './models/index'
 export default class DATABASE {
-  public static DB = pg()(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${
-      NODE_ENV === TEST ? DB_HOST_TEST : DB_HOST
-    }:${DB_PORT ? +DB_PORT : 5432}/${
-      NODE_ENV === TEST ? DB_NAME_TEST : DB_NAME
-    }`
-  )
+  public static DB = pg()(this.getCN())
 
   public static async initDatabase(): Promise<void> {
     if (NODE_ENV !== DEVELOPMENTMODE) {
+      console.log(
+        `Database ${DB_NAME} is not in development mode try create models`
+      )
       await Models.initModels()
       console.log('all tables created')
     }
   }
+
+  private static getCN(): string {
+    return `postgres://${DB_USER}:${decodeURIComponent(`${DB_PASSWORD}`)}@${
+      NODE_ENV === TEST ? DB_HOST_TEST : DB_HOST
+    }:${DB_PORT ? +DB_PORT : 5432}/${
+      NODE_ENV === TEST ? DB_NAME_TEST : DB_NAME
+    }`
+  }
 }
+
 export type PgType = typeof DATABASE.DB
