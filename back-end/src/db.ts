4624990fbd5618874bf529923a2afef3077269c8
MarcoAR1 <marcorivero.mr26@gmail.com>
2021-08-31 15:39:31 -0700

    .

--- back-end/src/db.ts
+++ back-end/src/db.ts
@@ -1,35 +1,16 @@
-import { DEVELOPMENTMODE, TEST } from './constants/constants'
+import { DEVELOPMENTMODE } from './constants/constants'
 import * as pg from 'pg-promise'
-import {
-  DB_HOST,
-  DB_HOST_TEST,
-  NODE_ENV,
-  DB_USER,
-  DB_PASSWORD,
-  DB_NAME_TEST,
-  DB_PORT,
-  DB_NAME,
-} from './utils/config'
+import { NODE_ENV } from './utils/config'
 import Models from './models/index'
 export default class DATABASE {
-  public static DB = pg()(this.getCN())
+  public static DB = pg()(`${process.env.DATABASE_URL}`)
 
   public static async initDatabase(): Promise<void> {
     if (NODE_ENV !== DEVELOPMENTMODE) {
-      console.log(
-        `${NODE_ENV} mode try create models`
-      )
+      console.log(`${NODE_ENV} mode try create models`)
       await Models.initModels()
       console.log('all tables created')
     }
-  }
-
-  private static getCN(): string {
-    return `postgres://${DB_USER}:${DB_PASSWORD}@${
-      NODE_ENV === TEST ? DB_HOST_TEST : DB_HOST
-    }:${DB_PORT ? +DB_PORT : 5432}/${
-      NODE_ENV === TEST ? DB_NAME_TEST : DB_NAME
-    }`
   }
 }
 
